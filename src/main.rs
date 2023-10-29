ff87c9b25d58c38fb62707f51bbad57c5bb29272
Richard Rast <richard.rast@trinetx.com>
2023-10-29 17:27:27 -0400

    chore: move giant comment to notes file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::env;
 use std::time::Instant;
+
+use clap::{Parser, Subcommand};
 
 use crate::cube::{Cube, Facelet};
 use crate::full_solve::{optimal_solve, HeuristicType};
 use crate::moves::{flipped, nice_write, CanFullMove, FullMove};
-use crate::scramble::{scramble_ofl, full_scramble, scramble_cfl};
+use crate::scramble::{full_scramble, scramble_cfl, scramble_ofl};
 
 mod cube;
 mod full_solve;
@@ -13,81 +14,6 @@ mod orr_solve;
 mod pos_solve;
 mod scramble;
 mod setup;
-
-/*
-Terminology we're going to fix:
-
-    CUBE -- the whole cube object itself
-    CUBELET / CUBIE -- the individual atomic block that moves around (in a pocket cube, only corner cubies)
-        the terms CUBELET and CUBIE will be used interchangeably
-    FACE -- one full "side" of the cube, which has four FACELETs on it
-    FACELET -- one little tile / sticker on the cube; on a pocket cube, each cubelet has 3 facelets
-
-    POSITION -- the position (not including orientation) of a cubie on the cube; there are eight possible
-        positions on the pocket cube, and any cubie can go in any position.
-
-        Relative to a fixed cubie (see below), we can say a particular cubie is in a correct position
-        or not.
-
-        Note: the statement "if we fix A, B is in the correct position" is symmetric between A and B;
-            that is, one is true iff the other is true
-
-    POSITIONALLY SOLVED -- a cube is positionally solved if every cubie is in a position where, if you
-        rotated them individually by whatever means, the puzzle would be solved
-
-        Three equivalents:
-        - There is a solution (possibly involving a screwdriver) to the cube which only changes
-            orientations of cubelets, and not positions
-        - For some cubelet, if we consider it fixed, every other cubelet is in the correct position
-        - For _every_ cubelet, if we consider it fixed, every other cubelet is in the correct position
-
-    ORIENTATION -- the "rotation" of a cubelet in its position. There are three possible orientations of
-        a cubelet in a given position.
-
-        Relative to a fixed cubie, we can say a cubelet is in the _correct_ orientation if its side facelet
-            (that is, the facelet which is on a side FACE) _should_ be on a side FACE. For instance if we
-            know the front "should" be green and the top "should" be white, then the side faces are orange
-            and red; so a cubie is in correct orientation if its red or orange facelet is actually on a
-            side face.
-
-        Relative to a fixed cubie, a cubelet has orientation 1 if it is clockwise-rotated one time from
-            the correct orienation. If has orientation 2 if it is counter-clockwise-rotated one time
-            from correct (or, equivalently, clockwise-rotated two times).
-
-            Because three turns bring you back "home" we take orienation mod 3 (so e.g. -1 is equivalent
-            to 2) and so on.
-
-        Note that all moves maintain the "total orientation" (mod 3) of the cube, and a solved cube has
-        total orientation 0; thus any configuration (again, possibly achieved using a screwdriver) whose
-        total orientation of something other than zero is unsolveable. This turns out to be the only
-        invariant that matters; every screwdriver-reachable configuration which has total orientation zero
-        is solveable.
-
-        Note: the statement "if A is fixed, then B has orientation P" is symmetric between A and B.
-                (where P could be zero, one, or two)
-
-    ORIENTATIONALLY SOLVED -- a cube is orientationally solved (relative to a fixed cubie; see below)
-        if every cubie has orientation zero.
-
-Note that unlike a 3x3 cube, there are no fixed centers; therefore we cannot say a particular
-piece is in the "correct" place or not because any solution to the cube has an equivalent solution, of
-the same length, which does not move or reorient a given piece. To see this, observe that the R and L
-moves result in _literally the same cube_, except you're holding it differently (that is, R and Lx
-result in the same cube). Thus you could replace the use of R with Lx; then transform the subsequent moves
-to sort of have the x "built in" (basically, the moves they would have been if you didn't do the x),
-and you now have a solution of the same length with R taken out.
-
-So in this way we can swap R and L; U and D; and F and B. Thus if your favorite cubie starts on the top,
-we can replace every U with D and never move it from the top, and so on.
-
-For me, the easiest moves are R/U/F; therefore, we'll canonically fix the BLD (back/left/down) corner
-cubie, which is equivalent to saying we want to find solutions only using R/U/F.
-
-As a nice bonus, once a particular cubie has its position and orientation fixed, everything else
-has a canonical correct position and orientation (relative to your fixed cubie). So when we want
-to know if a _cubelet_ has the correct position or orientation, we'll do it assuming the BLD
-(lower-left-back) cubie is fixed. Then there is a canonical answer.
-*/
 
 fn solve_input(input: &[FullMove]) {
     let cube = Cube::make_solved(Facelet::Green, Facelet::White).apply_many_full(input);
@@ -104,30 +30,79 @@ fn solve_input(input: &[FullMove]) {
     println!("Search took {:?}", elapsed);
 }
 
+#[derive(Subcommand, Copy, Clone, Debug)]
+enum ScrambleKind {
+    /// Performs a full scramble. All permutations are possible.
+    Full,
+    /// Performs an OFL scramble. The bottom layer will be correctly oriented.
+    OFL,
+    /// Performs a CFL scramble. The bottom layer will be completely solved.
+    CFL,
+    /// Performs an OLL scramble. The bottom and top layer will be correctly oriented.
+    OLL,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum CubeCommand {
+    Solve {
+        permutation: String,
+    },
+    Scramble {
+        #[clap(subcommand)]
+        kind: ScrambleKind,
+    },
+}
+
+#[derive(Parser, Debug)]
+struct Arguments {
+    #[clap(subcommand)]
+    cmd: CubeCommand,
+}
+
 fn main() -> Result<(), i32> {
-    let args: Vec<String> = env::args().skip(1).collect();
+    let args = Arguments::parse();
 
-    if args.len() == 0 {
-        let scramble = scramble_cfl();
-        println!("I made a scramble!");
+    match args.cmd {
+        CubeCommand::Solve { permutation } => {
+            let parsed = setup::parse_line(&permutation).map_err(|e| {
+                println!("Could not parse token {:?}", e);
+                1
+            })?;
 
-        let start = Instant::now();
-        let solution = optimal_solve(scramble, HeuristicType::Orr);
-        let elapsed = start.elapsed();
+            solve_input(&parsed);
+        }
+        CubeCommand::Scramble { kind } => {
+            let scramble = match kind {
+                ScrambleKind::Full => {
+                    println!("Doing a full scramble ...");
+                    full_scramble()
+                }
+                ScrambleKind::OFL => {
+                    println!("Doing an OFL scramble; first layer should be oriented ...");
+                    scramble_ofl()
+                }
+                ScrambleKind::CFL => {
+                    println!("Doing a CFL scramble; first layer should be completely solved ...");
+                    scramble_cfl()
+                }
+                ScrambleKind::OLL => {
+                    println!(
+                        "Doing an OLL scramble; first and second layer should be oriented ..."
+                    );
+                    unimplemented!()
+                }
+            };
 
-        println!("Full solution to scramble in {} moves", solution.len());
-        println!("Search took {:?}", elapsed);
+            let start = Instant::now();
+            let solution = optimal_solve(scramble, HeuristicType::Orr);
+            let elapsed = start.elapsed();
 
-        let steps = flipped(&solution);
-        println!("Scramble given by: {}", nice_write(&steps));
-    } else {
-        let input_line: String = args.join(" ");
-        let parsed = setup::parse_line(&input_line).map_err(|e| {
-            println!("Could not parse token {:?}", e);
-            1
-        })?;
+            println!("Full solution to scramble in {} moves", solution.len());
+            println!("Search took {:?}", elapsed);
 
-        solve_input(&parsed);
+            let steps = flipped(&solution);
+            println!("Scramble given by: {}", nice_write(&steps));
+        }
     }
 
     Ok(())
